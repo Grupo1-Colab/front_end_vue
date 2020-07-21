7eaa26ecd38d7f1b0928633f77a9d5a9326c8126
RicardoWebProject <luisrijhn09@gmail.com>
2020-07-20 18:34:32 -0600

    Agregada ruta al login

--- src/App.vue
+++ src/App.vue
@@ -228,6 +228,9 @@
         <span class="hidden-sm-and-down">Sistema FronTienda</span>
       </v-toolbar-title>      
       <v-spacer></v-spacer>
+      <v-btn icon :to="{name: 'Login'}">
+        <v-icon>login</v-icon>
+      </v-btn>
       <v-btn icon>
         <v-icon>logout</v-icon>
       </v-btn>
