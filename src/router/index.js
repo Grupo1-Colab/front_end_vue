904d0dfdbc723adba039deb076da9a9fddd8ec01
RicardoWebProject <luisrijhn09@gmail.com>
2020-07-20 18:24:48 -0600

    Agregada la ruta del login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+//Importar los Componentes
+import Home from '../views/Home.vue';
+import Categorias from '../components/Categoria.vue';
+import Login from '../components/Login.vue';
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,14 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/categoria',
+    name: 'Categoria',
+    component: Categorias
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
